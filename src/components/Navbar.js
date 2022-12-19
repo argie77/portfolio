8f286049455e6fd77f9fa52bcf245daf76944cbb
argie <argeline0729@yahoo.com>
2022-12-18 19:00:58 -0500

    useState is working

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,26 @@
 import "./NavbarStyle.css"
 
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from "react-router-dom"
+
+//category is Bars and icon is fa 
+import { FaBars, FaTimes } from "react-icons/fa"
 
 
 //The When we clicked the "Link" it will take us to our home page 
 const Navbar = () => {
+
+ //useState allows to have state variables in components
+const [click, setClick] = useState(false)
+const handleClick = () => setClick(!click)
+
+
   return (
     <div className="header">
         <Link to="/">
             <h1>Portfolio</h1>
         </Link>
-        <ul>
+        <ul className="nav-menu">
             <li>
                 <Link to="/">Home</Link>
             </li>
@@ -25,8 +34,16 @@ const Navbar = () => {
                 <Link to="/contact">Contact</Link>
             </li>
         </ul>
+        <div className="hamburger" onClick={handleClick}>
+            {click ? (
+            <FaTimes size={20} style={{color: "white"}} /> ) : (
+            <FaBars size={20} style={{ color: "white" }} /> )}
+                     
+        </div>
     </div>
   )
 }
 
 export default Navbar;
+
+//FaTimes is for the x icon to close 
