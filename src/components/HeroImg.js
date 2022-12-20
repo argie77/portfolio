498cfa5cac3369e828318d23759e4a1260701a9f
argie <argeline0729@yahoo.com>
2022-12-19 22:52:18 -0500

    background is good

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -2,13 +2,22 @@ import "./HeroImgStyles.css";
 
 import React from 'react';
 
-import twoImg from "../assets/two.jpeg"
+import animeImg from "../assets/anime.jpeg"
+import { Link } from "react-router-dom";
 
 const HeroImg = () => {
   return (
     <div className="hero">
         <div className="mask">
-            <img className="2.jpeg" src={twoImg} alt="twoImg"/>
+            <img className="anime-jpeg" src={animeImg} alt="animeImg"/>
+        </div>
+        <div className="content">
+            <p> Hello! I'm a Software Engineer</p>
+            <h1>My name is Argeline</h1>
+            <div>
+                <Link to="/project" className="btn">Projects</Link>
+                <Link to="/contact" className="btn btn-light">Contact</Link>
+            </div>
         </div>
     </div>
   )
