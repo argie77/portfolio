942a3108b8306b094127e69a3ff3f89ba1fdc4dd
argie <argeline0729@yahoo.com>
2023-01-02 12:26:44 -0500

    change picture

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -2,18 +2,18 @@ import "./HeroImgStyles.css";
 
 import React from 'react';
 
-import animeImg from "../assets/anime.jpeg"
+import monitorImg from "../assets/monitor.jpeg"
 import { Link } from "react-router-dom";
 
 const HeroImg = () => {
   return (
     <div className="hero">
         <div className="mask">
-            <img className="anime-jpeg" src={animeImg} alt="animeImg"/>
+            <img className="monitor-jpeg" src={monitorImg} alt="monitorImg"/>
         </div>
         <div className="content">
-            <p> Hello! I'm a Software Engineer</p>
-            <h1>My name is Argeline</h1>
+            <p> HeLLO, I'M ARGELINE</p>
+            <h1>FULLSTACK DEVELOPER </h1>
             <div>
                 <Link to="/project" className="btn">Projects</Link>
                 <Link to="/contact" className="btn btn-light">Contact</Link>
