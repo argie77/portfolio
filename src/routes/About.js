9d652fcba7e4dd932d4540d452d99edf12a89871
argie <argeline0729@yahoo.com>
2023-01-08 02:31:53 -0500

    data is showing up

--- src/routes/About.js
+++ src/routes/About.js
@@ -3,6 +3,7 @@ import React from 'react'
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import HeroImg2 from '../components/HeroImg2';
+import AboutContent from '../components/AboutContent';
 
 
 const About = () => {
@@ -10,6 +11,7 @@ const About = () => {
     <div>
       <Navbar/>
       <HeroImg2 heading="ABOUT" text="Software Engineer"/>
+      <AboutContent />
       <Footer/>
     </div>
   )
