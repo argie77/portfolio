9ef226a06cf79ea66392780b207223457382b439
argie <argeline0729@yahoo.com>
2023-01-08 01:23:53 -0500

    working to put projects in project page

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -8,22 +8,17 @@ import { NavLink } from "react-router-dom"
 
 const WorkCard = () => {
   return (
-    <div className="work-contaier">
-        <h1 className="project-heading">Projects</h1>
-        <div className="project-container">
-            <div className="project-card">
-                <img src={pro1} alt="image" />
-                <h2 className="project-title">Weather app</h2>
-                <div className="pro-details">
-                    <p>This is text</p>
-                    <div className="pro-btns">
-                        <NavLink to="https://62ba3f36360f320a1ca97767--thunderous-maamoul-f4789a.netlify.app/" className="btn">View</NavLink>
-                        <NavLink to="https://github.com/argie77/weather-app" className="btn">Source</NavLink>
-                    </div>
-                </div>
-            </div>
+    <div className="project-card">
+    <img src={pro1} alt="image" />
+    <h2 className="project-title">Weather app</h2>
+    <div className="pro-details">
+        <p>This is text</p>
+        <div className="pro-btns">
+            <NavLink to="https://62ba3f36360f320a1ca97767--thunderous-maamoul-f4789a.netlify.app/" className="btn">View</NavLink>
+            <NavLink to="https://github.com/argie77/weather-app" className="btn">Source</NavLink>
         </div>
     </div>
+</div>
   )
 }
 
---
+++ src/components/work.js
@@ -0,0 +1,20 @@
+import "./WorkCardStyles.css"
+
+import React from 'react'
+import pro1 from "../assets/weather.png"
+import { NavLink } from "react-router-dom"
+
+
+
+const Work = () => {
+  return (
+    <div className="work-contaier">
+        <h1 className="project-heading">Projects</h1>
+        <div className="project-container">
+           
+        </div>
+    </div>
+  )
+}
+
+export default Work
